848741542471783cf2892aa3b09d354ed01a6d17
AsierS <asiersanchezpalacio@gmail.com>
2020-03-02 13:26:23 +0100

    gondnodifngo

--- src/ejercicio/basico/principiantes/PasisesPatata.java
+++ src/ejercicio/basico/principiantes/PasisesPatata.java
@@ -11,9 +11,34 @@ public class PasisesPatata {
 		boolean[] paisesPatatizados = {false, false, false, false};
 		int[] numeroPatatas = {0, 0, 0, 0};
 		boolean hayInfectados = false;
-		int numeroInfectador;
+		int numeroInfectados;
+		int numeroInfectadosTotal=0;
+		Scanner sc = new Scanner(System.in);
 
-		
+		for (int i=0; i < PAISES.length; i++) {
+			System.out.println("¿Hay infectados de patata en " + PAISES[i] +"? True si hay, false si no.");
+			hayInfectados=Boolean.getBoolean(sc.nextLine());
+			System.out.println(hayInfectados);
+			if (hayInfectados==true) {
+				paisesPatatizados[i]=true;
+				System.out.println("¿Cuántos infectados de patata hay en " + PAISES[i] +"?");
+				numeroPatatas=sc.nextLine();
+				numeroInfectadosTotal += numeroInfectados;
+			} else if(hayInfectados==false) {
+				paisesPatatizados[i]=false;
+			}
+		}
+		for (int i=0; i < PAISES.length; i++) {
+			if (paisesPatatizados[i]==true) {
+				System.out.println(PAISES[i]+" está infectado.");
+				System.out.println("Aquí hay "+numeroPatatas[i]+" infectados.");
+			} else {
+				System.out.println(PAISES[i]+"no está infectado.");
+			}
+			
+				
+		}
+			
 		//paisesPatatizados[1]=true;
 		//numeroInfectados[1]=5;
 		
