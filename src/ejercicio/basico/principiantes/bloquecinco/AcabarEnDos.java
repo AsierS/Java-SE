abceadeb23e887afb3410cf3907cf300373718e4
AsierS <asiersanchezpalacio@gmail.com>
2020-03-03 13:57:48 +0100

    iuh

---
+++ src/ejercicio/basico/principiantes/bloquecinco/AcabarEnDos.java
@@ -0,0 +1,10 @@
+package ejercicio.basico.principiantes.bloquecinco;
+
+public class AcabarEnDos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ejercicio/basico/principiantes/bloquecinco/GradosTemperatura.java
@@ -0,0 +1,26 @@
+package ejercicio.basico.principiantes.bloquecinco;
+
+import java.util.Scanner;
+
+public class GradosTemperatura {
+
+	public static void main(String[] args) {
+		// lee una temperatura expresada en grados centígrados y la convierte a grados kelvin. 
+		
+		Scanner sc = new Scanner(System.in);
+		do {
+			
+		
+		System.out.println("Introduce grados centígrados: ");
+	    float gradosC = sc.nextInt();
+	    float gradosK=gradosC+273;
+	    System.out.println(gradosC+" ºC -> "+gradosK+"ºK.");
+
+	    System.out.println("¿Quieres salir? S para sí.");
+	    String car =sc.nextLine();
+        } while(true);
+		
+	    //sc.close();
+	}
+
+}
