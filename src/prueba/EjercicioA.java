7ddb62c60370a737de7a239904c19f934e328c08
AsierS <asiersanchezpalacio@gmail.com>
2020-03-04 13:44:39 +0100

    Ejercicios examen

--- src/prueba/EjercicioA.java
+++ src/prueba/EjercicioA.java
@@ -12,6 +12,11 @@ public class EjercicioA {
 
 	public static void main(String[] args) {
 
+		int alturaTotal=0;
+		int alturaMedia=0;
+		int alturaMax=0;
+		int alturaMin=999999;
+		
 		System.out.println("Empezamos");
 		int[] alturas = new int[5];
 		Scanner sc = new Scanner(System.in);
@@ -21,13 +26,25 @@ public class EjercicioA {
 			System.out.println("Dime un numero:");
 			int altura = Integer.parseInt(sc.nextLine());
 			alturas[i] = altura;
+			alturaTotal=(int) (alturaTotal+alturas[i]);
+			if(alturas[i]>alturaMax) {
+				alturaMax=alturas[i];
+			}
+			
+			if(alturas[i]<alturaMin) {
+				alturaMin=alturas[i];
+			}
 
-			// TODO calcular media
+			
 			// TODO altura maxima
 			// TODO altura minima
 
 		} // for
-
+		
+		alturaMedia=alturaTotal/alturas.length;
+		System.out.println("La altura media es "+alturaMedia+".");
+		System.out.println("La máxima es "+alturaMax+".");
+		System.out.println("La mínima es "+alturaMin+".");
 		System.out.println("Agur Venur");
 
 		sc.close();
